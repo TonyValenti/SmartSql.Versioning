beea27c205585bc20e04cc2e5fa9e1739218069d
Tony Valenti <tony.valenti@gmail.com>
2016-02-12 12:51:54 -0600

    Added BloodType and Sex

---
+++ src/SmartSql.Versioning.Samples.Entities/Data/Medical/BloodType.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using SmartSql.Versioning;
+
+namespace SmartSql.Versioning.Samples.Entities.Data {
+    //Data Access--------------------------------------------
+    public partial class BloodTypeController : EntityValueMemberController<BloodTypeInstance, BloodType> {
+
+    }
+
+    public partial class BloodTypeInstance : Instance<BloodType> {
+
+    }
+
+    public partial class BloodType : EntityRevision<BloodTypeInstance, BloodTypeValue> {
+        
+    }
+
+    public enum BloodTypeValue {
+        Unknown                 =   0000,
+        APositive               =   1100,
+        ANegative               =   1200,
+        BPositive               =   2100,
+        BNegative               =   2200,
+        ABPositive              =   3100,
+        ABNegative              =   3200,
+        OPositive               =   4100,
+        ONegative               =   4200,
+    }
+
+    public partial class DataContext : DbContext  {
+        public DbSet<BloodTypeInstance> BloodType { get; set; }
+        public DbSet<BloodType> BloodTypeRevisions { get; set; }
+    }
+
+    //WebApi Access--------------------------------------------
+    public class BloodTypeParameters : ValueParameter<BloodTypeValue> {
+
+    }
+
+    public class BloodTypeResponse : EntityMemberResponse<BloodTypeValue> {
+
+    }
+
+    public class BloodTypeApiController : EntityValueMemberApiController<
+      BloodTypeController,
+      BloodTypeInstance, BloodType,
+      BloodTypeParameters, BloodTypeParameters,
+      BloodTypeResponse
+      > {
+
+    }
+}
---
+++ src/SmartSql.Versioning.Samples.Entities/Data/Medical/Sex.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using SmartSql.Versioning;
+
+namespace SmartSql.Versioning.Samples.Entities.Data {
+    //Data Access--------------------------------------------
+    public partial class SexController : EntityValueMemberController<SexInstance, Sex> {
+
+    }
+
+    public partial class SexInstance : Instance<Sex> {
+
+    }
+
+    public partial class Sex : EntityRevision<SexInstance, SexValue> {
+        
+    }
+
+    public enum SexValue {
+        Unknown                 =   0000,
+        Male                    =   1000,
+        Female                  =   2000,
+    }
+
+    public partial class DataContext : DbContext  {
+        public DbSet<SexInstance> Sex { get; set; }
+        public DbSet<Sex> SexRevisions { get; set; }
+    }
+
+    //WebApi Access--------------------------------------------
+    public class SexParameters : ValueParameter<SexValue> {
+
+    }
+
+    public class SexResponse : EntityMemberResponse<SexValue> {
+
+    }
+
+    public class SexApiController : EntityValueMemberApiController<
+      SexController,
+      SexInstance, Sex,
+      SexParameters, SexParameters,
+      SexResponse
+      > {
+
+    }
+}
